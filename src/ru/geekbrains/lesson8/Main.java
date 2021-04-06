34bf105ee7a3b5e19f3aef4e2ded8f76e6895dc7
AlexaNovikova <69957036+AlexaNovikova@users.noreply.github.com>
2021-04-06 11:32:59 +0300

    Домашняя работа к восьмому уроку.

---
+++ src/ru/geekbrains/lesson8/Main.java
@@ -0,0 +1,39 @@
+package ru.geekbrains.lesson8;
+
+import java.util.Random;
+
+public class Main {
+    static Random random;
+
+    public static void main(String[] args) {
+//        random= new Random();
+//       ChainingHashMap<Integer, String> chm = new ChainingHashMap<>(5);
+//       chm.put(1, "one");
+//       chm.put(2, "two");
+//       chm.put(3, "three");
+//        chm.put(5, "five");
+//        chm.put(6, "six");
+//        System.out.println("size: "+chm.size());
+//        System.out.println(chm);
+//        System.out.println(chm.remove(5));
+//        System.out.println(chm.remove(4));
+//        System.out.println(chm);
+//        System.out.println("size: "+chm.size());
+
+        LinearProbingHashMap<Integer, String> lphm = new LinearProbingHashMap<>(19);
+        lphm.put(1, "one");
+        lphm.put(2, "two");
+        lphm.put(3, "three");
+        lphm.put(6, "two");
+        lphm.put(8, "three");
+        System.out.println(lphm.gey(2));
+        System.out.println(lphm);
+        System.out.println(lphm.size());
+        lphm.remove(2);
+        System.out.println(lphm);
+        System.out.println(lphm.gey(2));
+        lphm.put(2, "two");
+        System.out.println(lphm);
+
+    }
+}
